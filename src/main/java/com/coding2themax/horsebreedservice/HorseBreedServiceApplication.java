d63833aa8dbf5e3b8c9940a6aaf9d5243b3ee98b
max quinones <maximilian.quinones@gmail.com>
2023-01-01 14:40:26 -0700

    init java

---
+++ src/main/java/com/coding2themax/horsebreedservice/HorseBreedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coding2themax.horsebreedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseBreedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseBreedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding2themax/horsebreedservice/HorseBreedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding2themax.horsebreedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseBreedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
